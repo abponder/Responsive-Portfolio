b57890282439445126e619b1e9e3779fb2f00243
alexbponder@gmail.com <alexbponder@gmail.com>
2018-03-06 23:05:54 -0800

    add 1st

---
+++ assets/js/Script.js
@@ -0,0 +1,26 @@
+// Declare x, give it the value of 5
+// Declare x, give it the value of 5
+
+function OpenPortfolio() {
+
+    urlval = "portfolio.html"
+    newWin = window.open(urlval, 'newHWin');
+    newWin.focus();
+    }
+    function OpenContact() {
+
+        urlval = "contact.html"
+        newWin = window.open(urlval, 'newHWin');
+        newWin.focus();
+        }
+    function OpenAbout() {
+
+        urlval = "index.html"
+        newWin = window.open(urlval, 'newHWin');
+        newWin.focus();
+        }
+
+        
+
+
+
